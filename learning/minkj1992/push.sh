49c3c976154cccb1311aa004c8e1fa8f213fc14a
minkj1992 <minkj1992@gmail.com>
2020-12-05 13:37:13 +0900

    shell
    Test: shell is ok
    
    Test: shell is ok

---
+++ learning/minkj1992/push.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mPush to forked repo and origin repo to GitHub...\033[0m\n"
+
+# Add changes to git.
+
+git add .
+
+# Commit changes.
+msg="Update: study $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# push to forked my repo
+git push origin main
+
+
+# push to origin repo
+git fetch upstream
+git checkout upstream/leoo
+git merge main
+git push origin upstream/leoo
+git push
