e6bffb2e8307ea29d4e7a89eccbd403c5c9ee822
jeju5 <>
2020-12-04 21:42:55 -0600

    tab to spaces

--- learning/jeju5/modern-react-with-redux/sandbox/seasons/src/index.js
+++ learning/jeju5/modern-react-with-redux/sandbox/seasons/src/index.js
@@ -3,33 +3,45 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 
-	// constructor is automatically called with props
-	constructor(prop) {
-		console.log("const")
-		super(prop);
-		this.state = { latitude : null };
-		
-		window.navigator.geolocation.getCurrentPosition(
-			position => {
-					this.setState({
-						latitude: position.coords.latitude
-					})
-			},
-			err => console.log(err)
-		)
-	}
+  // constructor is automatically called with props
+  constructor(prop) {
+    console.log("const")
+    super(prop);
+    this.state = {
+      latitude : null,
+      errorMsg : null
+    };
+    
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+          this.setState({
+            latitude: position.coords.latitude
+          })
+      },
+      err => {
+        this.setState({
+          errorMsg: err.message
+        })
+      }
+    )
+  }
 
-	render () {
-		console.log("render")
-		return (
-			<div>
-				Latitude: {this.state.latitude}
-			</div>
-		);
-	}
+  render () {
+    console.log("render")
+
+    const errorBar = this.state.errorMsg ? "Error: " + this.state.errorMsg : null;
+
+    return (
+      <div>
+        Latitude: {this.state.latitude}
+        <br />
+        {errorBar}
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
-	<App />,
-	document.querySelector('#root')
+  <App />,
+  document.querySelector('#root')
 )
