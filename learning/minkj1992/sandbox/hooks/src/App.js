9267eeaacc8db9a0b44b6d3d1bde455a06353e93
minkj1992 <minkj1992@gmail.com>
2020-12-07 19:17:42 +0900

    [React-Hooks] useEffect: useTitle

--- learning/minkj1992/sandbox/hooks/src/App.js
+++ learning/minkj1992/sandbox/hooks/src/App.js
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from 'react';
 
-const App = () => {
-  const sayHello = () => console.log('hello');
-  const [num, setNum] = useState(0);
-  const [num2, setNum2] = useState(0);
-  // 1. init + every(num, num2) changed
-  // useEffect(sayHello);
-  // 2. only init
-  // useEffect(sayHello, []);
-  // 3. init + every num changed
-  useEffect(sayHello, [num]);
+const useTitle = (initTitle) => {
+  const [title, setTitle] = useState(initTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector('title');
+    htmlTitle.innerText = title;
+  };
 
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
+
+const App = () => {
+  const titleUpdater = useTitle('Loading...');
+  setTimeout(() => titleUpdater("Leoo's Updater"), 3000);
   return (
     <div className="App">
-      <button onClick={() => setNum(num + 1)}>{num}</button>
-      <button onClick={() => setNum2(num2 + 1)}>{num2}</button>
+      <h1>Hi Leoo 😄</h1>
     </div>
   );
 };
