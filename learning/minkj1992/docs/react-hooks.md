9e37dc37bca4d1691e41dc4a46d8b48528329c4b
minkj1992 <minkj1992@gmail.com>
2020-12-08 22:38:15 +0900

    [React-Hooks] Add: usePreventLeave()

--- learning/minkj1992/docs/react-hooks.md
+++ learning/minkj1992/docs/react-hooks.md
@@ -215,3 +215,40 @@ const App = () => {
 
 export default App;
 ```
+
+- usePreventLeave()
+```js
+import React from 'react';
+
+const usePreventLeave = (msg = '') => {
+  const listener = (e) => {
+    console.log(msg);
+    // 표준에 따라 기본 동작 방지
+    e.preventDefault();
+    // Chrome에서는 returnValue 설정이 필요함
+    e.returnValue = '';
+  };
+
+  const enablePrevent = () => window.addEventListener('beforeunload', listener);
+  const disablePrevent = () =>
+    window.removeEventListener('beforeunload', listener);
+
+  return { enablePrevent, disablePrevent };
+};
+
+const App = () => {
+  const msg = '정말 떠나시고 싶으세요? 😇'; // Chrome removed support for custom message in ver 51
+  const { enablePrevent, disablePrevent } = usePreventLeave(msg);
+
+  return (
+    <div className="App">
+      <h1>usePreventLeave😄</h1>
+      <button onClick={enablePrevent}>Protect</button>
+      <button onClick={disablePrevent}>unProtect</button>
+    </div>
+  );
+};
+
+export default App;
+
+```
--- learning/minkj1992/sandbox/hooks/src/App.js
+++ learning/minkj1992/sandbox/hooks/src/App.js
@@ -1,33 +1,30 @@
 import React from 'react';
 
-const useConfirm = (msg = '', onConfirm, onCancel) => {
-  if (!onConfirm && typeof onConfirm !== 'function') {
-    return;
-  }
-
-  // not mandatory
-  if (onCancel && typeof onCancel !== 'function') {
-    return;
-  }
-
-  const confirmAction = () => {
-    if (window.confirm(msg)) {
-      onConfirm();
-    } else {
-      onCancel();
-    }
+const usePreventLeave = (msg = '') => {
+  const listener = (e) => {
+    console.log(msg);
+    // 표준에 따라 기본 동작 방지
+    e.preventDefault();
+    // Chrome에서는 returnValue 설정이 필요함
+    e.returnValue = '';
   };
-  return confirmAction;
+
+  const enablePrevent = () => window.addEventListener('beforeunload', listener);
+  const disablePrevent = () =>
+    window.removeEventListener('beforeunload', listener);
+
+  return { enablePrevent, disablePrevent };
 };
 
 const App = () => {
-  const deleteWorld = () => console.log('Deleteing the World...');
-  const abort = () => console.log('Aborted');
-  const confirmDelete = useConfirm('Are u sure?', deleteWorld, abort);
+  const msg = '정말 떠나시고 싶으세요? 😇'; // Chrome removed support for custom message in ver 51
+  const { enablePrevent, disablePrevent } = usePreventLeave(msg);
+
   return (
     <div className="App">
-      <h1>useConfirm😄</h1>
-      <button onClick={confirmDelete}>Delete the world</button>
+      <h1>usePreventLeave😄</h1>
+      <button onClick={enablePrevent}>Protect</button>
+      <button onClick={disablePrevent}>unProtect</button>
     </div>
   );
 };
