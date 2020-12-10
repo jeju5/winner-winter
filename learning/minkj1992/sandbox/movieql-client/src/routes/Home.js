4ba4d96dc4873d9740b67445cd41c2f2d6e1502b
minkj1992 <minkj1992@gmail.com>
2020-12-10 22:36:03 +0900

    [GraphQL-Client] Add: GET MOVIES Query

--- learning/minkj1992/sandbox/movieql-client/src/routes/Home.js
+++ learning/minkj1992/sandbox/movieql-client/src/routes/Home.js
@@ -1,3 +1,25 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+const Home = () => {
+  const { loading, err, data } = useQuery(GET_MOVIES);
+  if (loading) {
+    console.log(err);
+    return 'loading ...';
+  }
+  if (data && data.movies) {
+    console.log(err);
+    return data.movies.map((m, i) => <h1 key={i}>{m.id}</h1>);
+  }
+};
+
+export default Home;
