43ec2d1ae6213efbedb6384dab5887e7c2bdeec2
minkj1992 <minkj1992@gmail.com>
2020-12-10 22:21:42 +0900

    [GraphQL-Client] Add: RApollo Client

--- learning/minkj1992/sandbox/movieql-client/src/index.js
+++ learning/minkj1992/sandbox/movieql-client/src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider } from '@apollo/client';
+import client from './apollo';
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
