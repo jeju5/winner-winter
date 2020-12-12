1786539c287271d994204f4df5e731826058f17c
minkj1992 <minkj1992@gmail.com>
2020-12-12 14:38:20 +0900

    [NodeJs Http] Add: html in nodeJs

--- learning/minkj1992/sandbox/nodeBasic/server.js
+++ learning/minkj1992/sandbox/nodeBasic/server.js
@@ -1,12 +1,25 @@
 import http from 'http';
+import fs from 'fs';
 
-http
+const baseDir = './home.html';
+const server = http
   .createServer((req, res) => {
     console.log('Hello visitor');
-
-    res.write('<h1>Hello Node</h1>');
-    res.end('<p>Hello Server</p>');
+    fs.readFile(baseDir, (err, data) => {
+      if (err) {
+        throw err;
+      }
+      // buffer end
+      res.end(data);
+    });
   })
-  .listen(8080, () => {
-    console.log('Server is running at 127.0.0.1:8080');
-  });
+  .listen(8080);
+
+// add event listener
+server.on('listening', () => {
+  console.log('Server is running at http://127.0.0.1:8080');
+});
+
+server.on('error', (err) => {
+  console.error(err);
+});
