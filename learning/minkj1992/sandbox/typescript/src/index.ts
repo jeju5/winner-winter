f06a27d2083297f9eab9e849c0bf02112a1c9144
minkj1992 <minkj1992@gmail.com>
2020-12-15 21:20:16 +0900

    [Typescript] Ts Class

--- learning/minkj1992/sandbox/typescript/src/index.ts
+++ learning/minkj1992/sandbox/typescript/src/index.ts
@@ -1,19 +1,22 @@
-interface Person {
-  name: string;
-  age: number;
-  gender: string;
+class Person {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
+
+const minWook = new Person('minkj1992', 29, 'male');
 
 const sayHi = (p: Person): string => {
   return `Hello ${p.name}, you are ${p.age}, you are a ${p.gender}`;
 };
 
-const person = {
-  age: 29,
-  name: 'minkj1992',
-  gender: 'male',
-};
+const person = {};
 
-console.log(sayHi(person));
+console.log(sayHi(minWook));
 
 export {};
