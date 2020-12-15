038b15dbe33f829464ef7222334ba35953c86063
minkj1992 <minkj1992@gmail.com>
2020-12-15 20:45:05 +0900

    [Typescript] 타입스크립트 선택적 변수

--- learning/minkj1992/sandbox/typescript/index.js
+++ learning/minkj1992/sandbox/typescript/index.js
@@ -1,2 +1,8 @@
-console.log('Hello ts');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHi = (name, age, gender) => {
+    console.log(`HEllo ${name}, you are ${age}, you are a ${gender}`);
+};
+const name = 'minkj1992', age = 29;
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- learning/minkj1992/sandbox/typescript/index.ts
+++ learning/minkj1992/sandbox/typescript/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello TS');
+const sayHi = (name, age, gender?) => {
+  console.log(`HEllo ${name}, you are ${age}, you are a ${gender}`);
+};
+
+const name = 'minkj1992',
+  age = 29;
+
+sayHi(name, age);
+
+export {};
